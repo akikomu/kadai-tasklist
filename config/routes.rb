7752bc00b592a6f13d802d5bc874f03ae4960fc6
akiko <acco.acco@outlook.jp>
2018-06-28 05:52:11 +0000

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  root to:'toppages#index'
+ 
+  #root to:'toppages#index'
+  root "tasks#index"
   
   get 'login', to:'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout' , to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   
   resources :tasks
 end
